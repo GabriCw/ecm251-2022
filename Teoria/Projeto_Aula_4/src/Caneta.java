a26f3bdc1718ec7d09bda7cd7813c1c6689939fe
Gabriel_Couto <gabricouto2305@gmail.com>
2022-03-09 11:09:22 -0300

    Criação de Classes com java

---
+++ Teoria/Projeto_Aula_4/src/Caneta.java
@@ -0,0 +1,13 @@
+public class Caneta {
+    //Caracteristicas da caneta - seus atributos
+    String modelo;
+    String cor;
+    double ponta;
+    int carga;
+
+    //Comportamento das canetas - seus métodos
+    void escrever(String texto){
+        System.out.println(texto);
+    }
+
+}
