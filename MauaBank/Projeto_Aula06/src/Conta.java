02b59fd5ca261621bcbac192b0f2784a2d42307f
Gabriel_Couto <gabricouto2305@gmail.com>
2022-03-24 11:01:12 -0300

    MauaBank inicio

---
+++ MauaBank/Projeto_Aula06/src/Conta.java
@@ -0,0 +1,55 @@
+public class Conta {
+    //Atributos da nossa classe
+    private int numero;
+    private double saldo;
+    private Cliente cliente;
+
+    //Construtor
+    public Conta(Cliente cliente, int numero){
+        this.numero = numero;
+        this.cliente = cliente;
+        saldo = 0;
+    }
+    
+    //Métodos da classe
+    public String visualizarSaldo(){
+        return String.format("R$ %.2f", saldo);
+    }
+
+    public boolean depositar(double valor){
+       // if(valor > 0){
+       // this.saldo = this.saldo + valor;
+       // return true;
+       // }
+       // else{
+       //     return false;
+        //}
+        if(valor < 0)
+             return false;
+
+        this.saldo += valor;
+        return true;
+    }
+
+    public boolean sacar(double valor){
+       // if(valor <= saldo){
+       // this.saldo = this.saldo - valor;
+       // }
+       if(valor > saldo) return false;
+       if(valor < 0) return false;
+       this.saldo -= valor;
+       return true;
+    }
+
+    public boolean transferirDinheiro(double valor, Conta destino){
+        if(!sacar(valor)) return false;
+        if(!destino.depositar(valor)) return false;
+        return true;
+    }
+
+    public String toString(){
+        return "Conta Numero:" +numero +
+        "\nSaldo:" +visualizarSaldo() +
+        "\nCliente:" +cliente.getNome();
+    }
+}
