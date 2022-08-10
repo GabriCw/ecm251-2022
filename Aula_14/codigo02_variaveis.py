611eefda56abe126e1810a12d4d66a935e50232e
Gabriel_Couto <gabricouto2305@gmail.com>
2022-08-10 11:15:53 -0300

    Introdução Python

---
+++ Aula_14/codigo02_variaveis.py
@@ -0,0 +1,11 @@
+numero1 = 10
+numero2 = 5
+resultado = numero1 + numero2
+
+print(f'A soma de {numero1} com {numero2} da {resultado}')
+
+numero1 = 'Gabriel'
+print(numero1)
+
+resultado = numero1 + numero2 #vai dar erro nessa linha!
+print(resultado)
