164bd12ec1892387648f74bdb4d857ab4a2d72b5
GabriCw <gabricouto2305@gmail.com>
2022-08-25 10:58:15 -0300

    Tratando excecoes

--- manipulacao_arquivos/main.py
+++ manipulacao_arquivos/main.py
@@ -1,12 +1,23 @@
 #código para manipulação de um arquivo
 
-arquivo = open("data/sistemas.txt", "a")
-continuar = True
+from traceback import print_stack
 
-while continuar:
-    os_name = input("Diga um OS (vazio para sair):")
-    if not os_name:
-        continuar = False
-        continue  #força parar o laço e testar de novo a condição inicial
-    arquivo.write(os_name +"\n")
-arquivo.close()
+
+try:
+    arquivo = open("data/sistemas.txt", "a")
+    continuar = True
+
+    while continuar:
+        os_name = input("Diga um OS (vazio para sair):")
+        if not os_name:
+            continuar = False
+            continue  #força parar o laço e testar de novo a condição inicial
+        arquivo.write(os_name +"\n")
+    arquivo.close()
+except FileNotFoundError:
+    print("Caminho não existe, favor verificar")
+except Exception:
+    print("Algo não esperado ocorreu:")
+    print_stack()
+finally:
+    print("Chegamos no final pessoal")
