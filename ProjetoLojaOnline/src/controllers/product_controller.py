02a63605aa34768fea880570e5a212452a650fe8
GabriCw <gabricouto2305@gmail.com>
2022-09-29 10:20:18 -0300

    T3 - Criação dos Try-Except

--- ProjetoLojaOnline/src/controllers/product_controller.py
+++ ProjetoLojaOnline/src/controllers/product_controller.py
@@ -6,7 +6,11 @@ class ProductController():
         #Carrega dados do produto
         self.products = [
             Product(name="Fifa 22", price="250.00"),
-            Product(name="The Last of Us 2", price="250.00")
+            Product(name="Forza Horizon 5", price="200.00"),
+            Product(name="Madden 22", price="250.00"),
+            Product(name="The Last of Us 2", price="250.00"),
+            Product(name="Shadow of the Tomb Raider", price="150.00"),
+            Product(name="Red Dead Redemption 2", price="200.00")
         ]
     
     def check_product(self, product):
