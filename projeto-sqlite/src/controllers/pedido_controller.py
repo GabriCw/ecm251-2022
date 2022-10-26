7486d576594d10b0560c8d970992b93bd598272c
GabriCw <gabricouto2305@gmail.com>
2022-10-26 10:12:06 -0300

    Inserir pedido e deletar pedido

--- projeto-sqlite/src/controllers/pedido_controller.py
+++ projeto-sqlite/src/controllers/pedido_controller.py
@@ -19,3 +19,9 @@ class PedidoController:
     
     def atualizar_pedido(self, pedido) -> bool:
         return PedidoDAO.get_instance().atualizar_pedido(pedido)
+    
+    def deletar_pedido(self, id) -> bool:
+        return PedidoDAO.get_instance().deletar_item(id)
+    
+    def inserir_pedido(self, pedido) -> None:
+        PedidoDAO.get_instance().inserir_pedido(pedido)
--- projeto-sqlite/tests/teste_controller_pedido.py
+++ projeto-sqlite/tests/teste_controller_pedido.py
@@ -1,4 +1,5 @@
 from src.controllers.pedido_controller import PedidoController
+from src.models.pedido import Pedido
 
 controller = PedidoController()
 
@@ -13,5 +14,19 @@ controller.atualizar_pedido(item_pedido)
 
 print("--------------------------------------")
 resutado = controller.pegar_pedido("001")
+for elemento in resutado:
+    print(elemento)
+
+print("--------------------------------------")
+pedido = Pedido(id="151", id_item="10", id_cliente="ZEZE", data_hora="26/10/2022", numero_pedido="1234", quantidade=2)
+controller.inserir_pedido(pedido)
+resutado = controller.pegar_pedido(numero_pedido="1234")
+for elemento in resutado:
+    print(elemento)
+print("--------------------------------------")
+for elemento in resutado:
+    controller.deletar_pedido(elemento.id)
+    
+resutado = controller.pegar_pedido(numero_pedido="1234")
 for elemento in resutado:
     print(elemento)
