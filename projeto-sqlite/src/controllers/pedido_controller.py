dedfbfc7a513bd3d8c6b1f040b5fd71d0e0de3fc
GabriCw <gabricouto2305@gmail.com>
2022-10-26 11:27:21 -0300

    Finalização do menu (funcinal)

--- projeto-sqlite/src/controllers/pedido_controller.py
+++ projeto-sqlite/src/controllers/pedido_controller.py
@@ -1,27 +1,28 @@
 from src.dao.pedido_dao import PedidoDAO
 from src.models.pedido import Pedido
 from src.controllers.item_controller import ItemController
-
 class PedidoController:
+
     def __init__(self) -> None:
         pass
-    
+
     def total_pedido(self, numero_pedido) -> float:
-        items_pedido = PedidoDAO.get_instance().get_itens(numero_pedido)
+        items_pedido = PedidoDAO.get_instance().pegar_pedido(numero_pedido)
         total = 0
-        for(item_id, quantidade) in items_pedido:
-            item = ItemController.pegar_item(item_id)
-            total += item.preco * quantidade
+        item_controller = ItemController()
+        for item in items_pedido:
+            item_elemento = item_controller.pegar_item(item.id_item)
+            total += item_elemento.preco * item.quantidade
         return total
-    
-    def pegar_pedido(self, numero_pedido) -> list [Pedido]:
+
+    def pegar_pedido(self, numero_pedido)-> list[Pedido]:
         return PedidoDAO.get_instance().pegar_pedido(numero_pedido)
-    
-    def atualizar_pedido(self, pedido) -> bool:
+
+    def atualizar_pedido(self, pedido)-> bool:
         return PedidoDAO.get_instance().atualizar_pedido(pedido)
-    
-    def deletar_pedido(self, id) -> bool:
+
+    def deletar_pedido(self, id) ->bool:
         return PedidoDAO.get_instance().deletar_item(id)
-    
-    def inserir_pedido(self, pedido) -> None:
+        
+    def inserir_pedido(self, pedido)->None:
         PedidoDAO.get_instance().inserir_pedido(pedido)
