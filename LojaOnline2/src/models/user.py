d05b6b00ca78bcb67c0dd525beb5c3f6b23dcf46
GabriCw <gabricouto2305@gmail.com>
2022-11-07 17:56:04 -0300

    inicio T4

---
+++ LojaOnline2/src/models/user.py
@@ -0,0 +1,26 @@
+################################
+#NOME: Gabriel dos Santos Couto#
+#RA: 20.00273-4                #
+################################
+
+class User():
+    def __init__(self, name, email, password, cpf):
+        self._email = email
+        self._name = name
+        self._password = password
+        self._cpf = cpf
+
+    def get_password(self):
+        return self._password
+
+    def get_name(self):
+        return self._name
+
+    def get_email(self):
+        return self._email
+
+    def get_cpf(self):
+        return self._cpf
+
+    def __str__(self)->str:
+        return f'User(name:{self.get_name()}, email:{self.get_email()}, password:{self.get_password()} cpf:{self.get_cpf()}'
